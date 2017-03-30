edb6bd0a010e170302dbe6da7d4b8e270121898c
pablofv <tramitesyregistraciones@gmail.com>
2017-03-30 14:26:48 -0300

    Replica de procesos sqlserver para estadísticas en oracle
    Encabezado del paquete, lo había subido vacío.

--- EST_PAQ_EJECUTAR_encabezado.sql
+++ EST_PAQ_EJECUTAR_encabezado.sql
@@ -1,2 +1,3 @@
 CREATE OR REPLACE PACKAGE EST_PAQ_EJECUTAR AS 
+    function ejecutar_proceso(f_desde in timestamp, f_hasta in timestamp, CAMARA in int, quieroRecalcular varchar2) return int;
 END EST_PAQ_EJECUTAR;
