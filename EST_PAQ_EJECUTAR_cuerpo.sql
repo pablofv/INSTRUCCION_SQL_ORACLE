b072db223075081b7a2ca801c705b087bee12fd7
pablofv <tramitesyregistraciones@gmail.com>
2017-04-03 10:52:50 -0300

    Replica de procesos sqlserver para estadísticas en oracle
    Cambios en la forma de calcular los inicios de procesos. En vez de tener
    una sola variable, tengo una variable para saber lo que hay antes de la
    fecha que yo quiero "DATOS_ANTES_DEL_INICIO" y otra variable donde
    cuento cuantos registros hay luego del inicio
    "DATOS_DESPUES_DEL_INICIO".
    Luego cambié el condicional, empiezo mirando si NO QUIERO RECALCULAR y
    HAY DATOS ANTES DEL INICIO, en ese caso significa que ya hay datos
    calculados pero estoy diciendo que no quiero recalcular, o sea que es
    una estadística ya calculada entonces devuelvo 1 que significa error.
    Sino, si NO QUIERO RECALCULAR y NO HAY REGISTROS POSTERIORES, PERO SI
    REGISTROS ANTERIORES, quiere decir que es una continuación de una
    estadística anterior, por lo cual devuelvo 0 (correcto) pero no obtengo
    un nuevo número de ejecución.
    Sino, si NO QUIERO RECALCULAR, NO HAY DATOS DESPUÉS DEL INICIO y NO HAY
    DATOS ANTES DEL INICIO, o bien QUIERO RECALCULAR, entonces obtengo un
    nuevo número de ejecución y retorno 0.

--- EST_PAQ_EJECUTAR_cuerpo.sql
+++ EST_PAQ_EJECUTAR_cuerpo.sql
@@ -1,24 +1,32 @@
 CREATE OR REPLACE PACKAGE body EST_PAQ_EJECUTAR AS 
     function ejecutar_proceso(f_desde in timestamp, f_hasta in timestamp, CAMARA in int, quieroRecalcular varchar2) return int is
-        hay_registros_anteriores int;
+        datos_antes_del_inicio int;
+        datos_despues_del_inicio int;
         v_proceso varchar2(30) := 'ejecutar_proceso';
-        retorno int;
+        v_retorno int;
     begin
-        select nvl(count(*), 0) into hay_registros_anteriores
+        select nvl(count(*), 0) into datos_antes_del_inicio
         from est_total_a
-        where ta_fecha between f_desde and f_hasta
+        where ta_fecha < f_desde
         and   ta_camara = CAMARA;
 
-        if upper(quieroRecalcular) = 'N' and hay_registros_anteriores > 0 then
-          return 1; -- no quiero recalcular pero hay registros, informar error de estadística ya calculada
-        elsif upper(quieroRecalcular) = 'N' and hay_registros_anteriores = 0 then
-          return 0;
-        elsif upper(quieroRecalcular) = 'S' then
+        select nvl(count(*), 0) into datos_despues_del_inicio
+        from est_total_a
+        where ta_fecha > f_desde
+        and   ta_camara = CAMARA;
+        
+
+        if upper(quieroRecalcular) = 'N' and datos_despues_del_inicio > 0 then
+          v_retorno := 1; -- no quiero recalcular pero hay registros, informar error de estadística ya calculada
+        elsif upper(quieroRecalcular) = 'N' and datos_despues_del_inicio = 0 and datos_antes_del_inicio > 0 then
+          v_retorno := 0;
+        else
           insert into est_fecha_de_procesos(fecha, camara) values (sysdate, CAMARA);
-          return 0; -- o bien quiero recalcular o no quiero recalcular y no hay registros previos, seguimos con el proceso
+          v_retorno := 0; -- o bien quiero recalcular o no quiero recalcular y no hay registros previos, seguimos con el proceso
         end if;
 
         select max(n_ejecucion) into est_paquete.v_numero_de_ejecucion from est_fecha_de_procesos;
+        return v_retorno;
     exception
       when others then
           est_paquete.inserta_error(m_error => DBMS_UTILITY.format_error_stack, nombre_proceso => v_proceso);
