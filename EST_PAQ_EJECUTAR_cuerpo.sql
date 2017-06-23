2074a0408ab733dd7e9f921f0b5e6e5cc751a7da
pablofv <tramitesyregistraciones@gmail.com>
2017-06-23 16:19:37 -0300

    Replica de procesos sqlserver para estadísticas en oracle
    Agregué una nueva variable,
    v_numero_est int;
    para saber que número de estadística corresponde a la ejecución actual.
    Luego agregué las asignaciones correspondientes a la variable antes
    mencionada, para luego modificar el insert a la tabla
    EST_FECHA_DE_PROCESOS, incluyendo el nuevo campo de número de
    estadística.

--- EST_PAQ_EJECUTAR_cuerpo.sql
+++ EST_PAQ_EJECUTAR_cuerpo.sql
@@ -4,6 +4,7 @@ CREATE OR REPLACE PACKAGE body EST_PAQ_EJECUTAR AS
         datos_despues_del_inicio int;
         v_proceso varchar2(30) := 'ejecutar_proceso';
         v_retorno int;
+        v_numero_est int;
     begin
         select nvl(count(*), 0) into datos_antes_del_inicio
         from est_total_a
@@ -17,18 +18,34 @@ CREATE OR REPLACE PACKAGE body EST_PAQ_EJECUTAR AS
         
 
         if upper(quieroRecalcular) = 'N' and datos_despues_del_inicio > 0 then
-          v_retorno := 1; -- no quiero recalcular pero hay registros, informar error de estadística ya calculada
+          -- no quiero recalcular pero hay registros del mismo período o posterior, informar error de estadística ya calculada
+          v_retorno := 1;
         elsif upper(quieroRecalcular) = 'N' and datos_despues_del_inicio = 0 and datos_antes_del_inicio > 0 then
+          -- no quiero recalcular no hay registros posteriores al período que estoy ejecutando, pero hay datos anteriores, estoy ejecutando un período siguiente
+          -- de una estadística empezada
           v_retorno := 0;
+          -- voy a mantener el mismo número de estadística
+          select nvl(max(NUMERO_ESTADISTICA), 0) into v_numero_est
+          from est_fecha_de_procesos
+          where camara = CAMARA;
         else
+          -- si no quiero recalcular, en este punto no tengo datos anteriores ni posteriores a lo que estoy calculando, por lo tanto es la primera, así que debe
+          -- darme null la consulta, con un nvl lo transformo a 0 y le sumo uno para empezar con la primera.
+          -- si quisiera recalcular, la lógica es la misma, quiero un número mas que el mayor que haya en la base, lo voy a calcular aquí por eso, es la misma
+          -- consulta que tendría que hacer adentro del if de si quiero recalcular.
+          select nvl(max(NUMERO_ESTADISTICA), 0) + 1 into v_numero_est
+          from est_fecha_de_procesos
+          where camara = CAMARA;
+
           if upper(quieroRecalcular) = 'S' then -- Si estoy recalculando, indico que no hay datos anteriores aunque los haya, porque en tal caso serían de otra estadística.
               datos_antes_del_inicio := 0;
           end if;
-          insert into est_fecha_de_procesos(fecha, camara) values (sysdate, CAMARA);
-          v_retorno := 0; -- o bien quiero recalcular o no quiero recalcular y no hay registros previos, seguimos con el proceso
+          insert into est_fecha_de_procesos(fecha, camara, NUMERO_ESTADISTICA) values (sysdate, CAMARA, v_numero_est);
+          v_retorno := 0; -- o bien quiero recalcular o no quiero recalcular y no hay registros previos ni posteriores, seguimos con el proceso
         end if;
 
         select max(n_ejecucion) into est_paquete.v_numero_de_ejecucion from est_fecha_de_procesos;
+        select max(NUMERO_ESTADISTICA) into est_paquete.v_numero_estadistica from est_fecha_de_procesos where camara = CAMARA;
         return v_retorno;
     exception
       when others then
