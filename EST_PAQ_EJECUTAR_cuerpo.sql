05e6108747365a3f2bf81725bf84b45cad3b0786
pablofv <tramitesyregistraciones@gmail.com>
2017-11-22 15:13:50 -0300

    Replica de procesos sqlserver para estadísticas en oracle
    El proceso inicial ha cambiado.
    Ahora tengo algunas funciones y procedimientos:
    existe_periodo: es una función que devuelve verdadero si la cantidad de
    filas que hay para el número de estadística, la cámara  y el rango de
    fechas indicados es mayor a cero, y sino devuelve falso. Es para saber
    si la estadística que estoy por calcular ya existe.
    periodo_inmediato_anterior: Devuelve verdadero si hay un registro en
    ejecuciones, con fecha hasta en el dia anterior al comienzo de la nueva
    estadística (lo que esto pretende, es identificar si existe el período
    inmediato anterior, por ej si ahora voy a calcular 2013, desde el 1 de
    enero, me fijo si hay una estadística que terminó el 31 de dic de 2012.
    Por supuesto si hiciera una estadística con 2 días de diferencia, el
    proceso interpretaría que no hay una estadística que la precede.
    obtener_estadistica: es un procedimiento que determina que número de
    estadística es la actual para la cámara y lo carga en una variable
    (est_paq_ejecutar.n_estadistica).
    
    La idea del proceso es: me fijo si existe el período que estoy
    solicitando, SI EXISTE, a la variable (guardada en la tabla
    est_variable_estadistica) le sumo uno, y ese valor lo almaceno en la
    variable est_paquete.v_numero_estadistica.
    SI NO EXISTE, me fijo si existe un periodo inmediato anterior, SI EXISTE
    asigno el valor de estadística que existía en la variable
    est_paquete.v_numero_estadistica, o sea continuo con el mismo número de
    estadística, porque es el período siguiente.
    SI NO EXISTE, hago lo mismo de antes, sumo uno al número de estadística,
    y guardo en la variable el valor.
    Grabo en la tabla de ejecuciones y retorno cero.

--- EST_PAQ_EJECUTAR_cuerpo.sql
+++ EST_PAQ_EJECUTAR_cuerpo.sql
@@ -1,57 +1,99 @@
 CREATE OR REPLACE PACKAGE body EST_PAQ_EJECUTAR AS 
-    function ejecutar_proceso(f_desde in timestamp, f_hasta in timestamp, CAMARA in int, quieroRecalcular varchar2, datos_antes_del_inicio in out int) return int is
-     --   datos_antes_del_inicio int;
-        datos_despues_del_inicio int;
-        v_proceso varchar2(30) := 'ejecutar_proceso';
-        v_retorno int;
+    -- Está función debe retornar verdadero (o cero si no existe el tipo boolean)
+    -- si el período que estoy queriendo calcular en el número de estadística que corresponda existe,
+    -- caso contrario deberá retornar falso (o uno).
+    function existe_periodo (f_desde in timestamp, f_hasta in timestamp, p_CAMARA in int) return boolean is
+      existe int;
+      v_proceso varchar2(30) := 'existe_periodo';
+      procedure obtener_estadistica is
+          v_proceso varchar2(30) := 'obtener_estadistica';
+      begin
+          -- Obtiene el valor de estadística actual.
+          select ve_valor into EST_PAQ_EJECUTAR.n_estadistica
+          from est_variable_estadistica
+          where ve_camara = p_CAMARA;
+      exception
+      when others then
+          est_paquete.inserta_error(m_error => DBMS_UTILITY.format_error_stack, nombre_proceso => v_proceso);
+      end obtener_estadistica;
     begin
-        select nvl(count(*), 0) into datos_antes_del_inicio
+        --cargamos la variable número de estadística
+        obtener_estadistica;
+
+        select nvl(count(*), 0) into existe
         from est_total_a
-        where ta_fecha < f_desde
-        and   ta_camara = CAMARA;
+        where ta_fecha between f_desde and f_hasta
+        and   ta_camara = p_CAMARA
+        and   ta_numero_estadistica = EST_PAQ_EJECUTAR.n_estadistica;
 
-        select nvl(count(*), 0) into datos_despues_del_inicio
-        from est_total_a
-        where ta_fecha > f_desde
-        and   ta_camara = CAMARA;
-
-        -- Quiero que cada corrida sea una nueva estadística
-        if upper(quieroRecalcular) = 'N' and datos_despues_del_inicio > 0 then
-          -- no quiero recalcular pero hay registros del mismo período o posterior, informar error de estadística ya calculada
-          v_retorno := 1;
-        elsif upper(quieroRecalcular) = 'N' and datos_despues_del_inicio = 0 and datos_antes_del_inicio > 0 then
-          -- no quiero recalcular no hay registros posteriores al período que estoy ejecutando, pero hay datos anteriores, estoy ejecutando un período siguiente
-          -- de una estadística empezada
-          v_retorno := 0;
-
-          select max(NUMERO_ESTADISTICA) into est_paquete.v_numero_estadistica -- Busco para la cámara, la última ejecución que haya, y dejo el mismo número de estadística
-          from EST_EJECUCIONES -- ya que es una continuación de ejecuciones anteriores.
-          where camara = CAMARA
-          and   n_ejecucion = (select max(n_ejecucion) from EST_EJECUCIONES);
-          insert into EST_EJECUCIONES(FECHA_PROCESO, camara, NUMERO_ESTADISTICA, FECHA_DESDE, FECHA_HASTA) values (sysdate, CAMARA, est_paquete.v_numero_estadistica, f_desde, f_hasta);
+        if existe <> 0 then
+          return true;
         else
-          -- si no quiero recalcular, en este punto no tengo datos anteriores ni posteriores a lo que estoy calculando, por lo tanto es la primera, así que debe
-          -- darme null la consulta, con un nvl lo transformo a 0 y le sumo uno para empezar con la primera.
-          -- si quisiera recalcular, la lógica es la misma, quiero un número mas que el mayor que haya en la base, lo voy a calcular aquí por eso, es la misma
-          -- consulta que tendría que hacer adentro del if de si quiero recalcular.
-              select nvl(max(NUMERO_ESTADISTICA), 0) + 1 into est_paquete.v_numero_estadistica --Busco para la cámara la última ejecución que haya, y a ese número 
-              from EST_EJECUCIONES -- de estadística le sumo uno, para empezar una nueva. Eventualmente, en la primer ejecución me devolverá null, con lo cual lo
-              where camara = CAMARA -- transformo en cero, para sumarle uno.
-              and   n_ejecucion = (select max(n_ejecucion) from EST_EJECUCIONES);
-
-          if upper(quieroRecalcular) = 'S' then -- Si estoy recalculando, indico que no hay datos anteriores aunque los haya, porque en tal caso serían de otra estadística.
-              datos_antes_del_inicio := 0;
-          end if;
-          insert into EST_EJECUCIONES(FECHA_PROCESO, camara, NUMERO_ESTADISTICA, FECHA_DESDE, FECHA_HASTA) values (sysdate, CAMARA, est_paquete.v_numero_estadistica, f_desde, f_hasta);
-          v_retorno := 0; -- o bien quiero recalcular o no quiero recalcular y no hay registros previos ni posteriores, seguimos con el proceso
+          return false;
         end if;
-
-        select max(n_ejecucion) into est_paquete.v_numero_de_ejecucion from EST_EJECUCIONES;
-        commit;
-        return v_retorno;
     exception
       when others then
           est_paquete.inserta_error(m_error => DBMS_UTILITY.format_error_stack, nombre_proceso => v_proceso);
+    end existe_periodo;
+
+    function periodo_inmediato_anterior (f_desde in timestamp, p_CAMARA in int) return boolean is
+        inmediato int;
+        v_proceso varchar2(30) := 'periodo_inmediato_anterior';
+    begin
+        -- Se fija si el período inmediato anterior se encuentra calculado.
+        select nvl(count(*), 0) into inmediato
+        from est_ejecuciones
+        where fecha_hasta = f_desde - 1
+        and   camara = p_camara;
+
+        if inmediato <> 0 then
+          return true;
+        else
+          return false;
+        end if;
+    exception
+      when others then
+          est_paquete.inserta_error(m_error => DBMS_UTILITY.format_error_stack, nombre_proceso => v_proceso);
+    end periodo_inmediato_anterior;
+
+    function ejecutar_proceso(f_desde in timestamp, f_hasta in timestamp, CAMARA in int) return int is
+        v_proceso varchar2(30) := 'ejecutar_proceso';
+    begin
+        --Controlo si ya calculé el período solicitado.
+        if existe_periodo(f_desde => f_desde, f_hasta => f_hasta, p_CAMARA => Camara) then
+          -- Sumo uno a la variable estadística.
+          update est_variable_estadistica
+          set ve_valor = ve_valor + 1
+          where ve_camara = camara;
+
+          select ve_valor into est_paquete.v_numero_estadistica
+          from est_variable_estadistica
+          where ve_camara = camara;
+        elsif periodo_inmediato_anterior(f_desde => f_desde, p_CAMARA => Camara) then
+          -- Dejo el mismo valor de estadística actual.
+          select ve_valor into est_paquete.v_numero_estadistica
+          from est_variable_estadistica
+          where ve_camara = camara;
+        else
+          -- Sumo uno a la variable estadística.
+          update est_variable_estadistica
+          set ve_valor = ve_valor + 1
+          where ve_camara = camara;
+
+          select ve_valor into est_paquete.v_numero_estadistica
+          from est_variable_estadistica
+          where ve_camara = camara;
+        end if;
+        -- inserto una nueva ejecución.
+        insert into EST_EJECUCIONES(FECHA_PROCESO, camara, NUMERO_ESTADISTICA, FECHA_DESDE, FECHA_HASTA) values (sysdate, CAMARA, est_paquete.v_numero_estadistica, f_desde, f_hasta);
+        --Obtengo el mayor número de ejecución y lo almaceno en la variable del paquete para tener acceso desde cualquier proceso.
+        select max(n_ejecucion) into est_paquete.v_numero_de_ejecucion from EST_EJECUCIONES;
+        commit;
+        return 0;
+    exception
+      when others then
+          est_paquete.inserta_error(m_error => DBMS_UTILITY.format_error_stack, nombre_proceso => v_proceso);
+          rollback;
           return 1;
     end ejecutar_proceso;
 END EST_PAQ_EJECUTAR;
