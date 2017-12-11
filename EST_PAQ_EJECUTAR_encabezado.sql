d13fcec87b6945eec440682dbbfea348cf809c24
pablofv <tramitesyregistraciones@gmail.com>
2017-12-11 11:34:36 -0300

    Replica de procesos sqlserver para estadísticas en oracle
    Agregado de la función en el encabezado del paquete para su definición.

--- EST_PAQ_EJECUTAR_encabezado.sql
+++ EST_PAQ_EJECUTAR_encabezado.sql
@@ -1,4 +1,5 @@
 CREATE OR REPLACE PACKAGE EST_PAQ_EJECUTAR AS
     n_estadistica int;
+    function periodo_inmediato_anterior (f_desde in timestamp, p_CAMARA in int) return boolean;
     function ejecutar_proceso(f_desde in timestamp, f_hasta in timestamp, CAMARA in int) return int;
 END EST_PAQ_EJECUTAR;
