08228eae44f92d9fdbc6db4735d7b6fad65b3a38
pablofv <tramitesyregistraciones@gmail.com>
2017-12-11 11:27:42 -0300

    Replica de procesos sqlserver para estadísticas en oracle
    Hice similar a instrucción, por empezar, ya no tengo que decir si hay
    que recalcular, el proceso se fija si ya hay una estadística de ese
    período calculada, si hay una inmediata anterior, y determina que número
    de estadística otorgar.
    Por lo tanto los parámetros de entrada del proceso general
    (calcular_estadistica_federal) son solo Desde y Hasta. Ejecutar_proceso
    agrega además el parámetro de cámara.
    El nombre del error lo cambié, ya que no se considera mas un error
    calcular un período existente en los cálculos. E introducimos, el
    proceso periodo_inmediato_anterior, el cual se fija si ya hay algo
    calculado anteriormente, pasa saber si hay que calcular el saldo inicial
    (esto en instrucción no lo tengo, porque arranco sin saldo en el año
    2008).

--- paquete_federal_cuerpo.sql
+++ paquete_federal_cuerpo.sql
@@ -1,15 +1,13 @@
 create or replace package body est_paquete_federal as
-
-    procedure calcular_estadistica_federal(desde in timestamp default to_timestamp('01/01/2008', 'dd/mm/yyyy'), hasta in timestamp, recalcular varchar2 default 'N') is
-      error_yaFueCalculado exception; -- excepcion para cuando quiero calcular algo que ya está calculado
-      hay_registros_anteriores int;
+    procedure calcular_estadistica_federal(desde in timestamp default to_timestamp('01/01/2008', 'dd/mm/yyyy'), hasta in timestamp) is
+      error_enEjecutarProceso exception; -- excepcion para cuando quiero calcular algo que ya está calculado
       v_proceso varchar2(30) := 'calcular_estadistica_federal';
     begin
-        if EST_PAQ_EJECUTAR.ejecutar_proceso(f_desde => desde, f_hasta => hasta, CAMARA => N_CAMARA, quieroRecalcular => recalcular, datos_antes_del_inicio => hay_registros_anteriores) = 1 then
+        if EST_PAQ_EJECUTAR.ejecutar_proceso(f_desde => desde, f_hasta => hasta, CAMARA => N_CAMARA) = 1 then
             /* 1-> error, 0-> correcto */
-            raise error_yaFueCalculado;
+            raise error_enEjecutarProceso;
         else
-            if hay_registros_anteriores = 0 then
+            if not EST_PAQ_EJECUTAR.periodo_inmediato_anterior (f_desde => desde, p_CAMARA => N_CAMARA) then
                 est_paquete.saldo_al_inicio(v_fechahasta => desde, id_cam => N_CAMARA);
             end if;
             est_paquete.ingresados(V_FECHADESDE => desde, V_FECHAHASTA => hasta, id_cam => N_CAMARA);
@@ -18,8 +16,8 @@ create or replace package body est_paquete_federal as
             est_paquete.calcula_salidos(finPeriodo => hasta, id_cam => N_CAMARA);
         end if;
     exception
-      when error_yaFueCalculado then
-          est_paquete.inserta_error(m_error => 'ESTADÍSTICA YA CALCULADA', nombre_proceso => v_proceso);
+      when error_enEjecutarProceso then
+          est_paquete.inserta_error(m_error => 'HUBO UN ERROR EJECUTANDO LA FUNCIÓN "EJECUTAR_PROCESO"', nombre_proceso => v_proceso);
       when others then
           est_paquete.inserta_error(m_error => DBMS_UTILITY.format_error_stack, nombre_proceso => v_proceso);
     end calcular_estadistica_federal;
