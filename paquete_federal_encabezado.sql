f544a3ab08fc2a7fa1fda6821b5273df14ee59e5
pablofv <tramitesyregistraciones@gmail.com>
2017-12-11 11:35:36 -0300

    Replica de procesos sqlserver para estadísticas en oracle
    Cambio en la definición de los parámetros del proceso, se suprimió
    "recalcular".

--- paquete_federal_encabezado.sql
+++ paquete_federal_encabezado.sql
@@ -1,4 +1,4 @@
 create or replace package est_paquete_federal as
     N_CAMARA NUMBER(2) := 8;
-    procedure calcular_estadistica_federal(desde in timestamp default to_timestamp('01/01/2008', 'dd/mm/yyyy'), hasta in timestamp, recalcular varchar2 default 'N');
+    procedure calcular_estadistica_federal(desde in timestamp default to_timestamp('01/01/2008', 'dd/mm/yyyy'), hasta in timestamp);
 end est_paquete_federal;
