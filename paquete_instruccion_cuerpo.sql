824d9edaaa70f3006ffde0a3c1c90f569dd8c0a8
pablofv <tramitesyregistraciones@gmail.com>
2017-12-15 13:42:05 -0300

    Replica de procesos sqlserver para estadísticas en oracle
    Sacamos la llamada al procedimiento que no existe más, REINGRESADOS.

--- paquete_instruccion_cuerpo.sql
+++ paquete_instruccion_cuerpo.sql
@@ -9,7 +9,6 @@ create or replace package body est_paquete_instruccion as
         else
             /* En instrucción empezaremos sin expedientes en trámite */
             est_paquete.ingresados_y_reingresados(V_FECHADESDE => desde, V_FECHAHASTA => hasta, id_cam => N_CAMARA);
-            est_paquete.reingresados(v_fechaDesde => desde, v_fechahasta => hasta, id_cam => N_CAMARA);
             --ELIMINAMOS TODO LO QUE NO SEA UN MOVIMIENTO DE INSTRUCCIÓN
             dejarSoloInstruccion;
             est_paquete.agrego_delito(id_cam => N_CAMARA);
