d7aa17c8c28fabeabf4a0b8ac291b1a9541a669a
pablofv <tramitesyregistraciones@gmail.com>
2018-04-19 17:48:19 -0300

    Replica de procesos sqlserver para estadísticas en oracle
    Los encabezados de los nuevos procesos.

--- paquete_instruccion_encabezado.sql
+++ paquete_instruccion_encabezado.sql
@@ -1,5 +1,9 @@
 create or replace package est_paquete_instruccion as
     N_CAMARA NUMBER(2) := 9;
     procedure calcular_estadistica_instr(desde in timestamp default to_timestamp('01/01/2008', 'dd/mm/yyyy'), hasta in timestamp default to_timestamp('31/12/2008', 'dd/mm/yyyy'));
+    procedure eliminarAsignacionesAntA2013;
     procedure dejarSoloInstruccion;
+    procedure dejarSoloInstruccion2;
+    procedure eliminarAnterioresA2008;
+    procedure eliminarFalsasAsignaciones;
 end est_paquete_instruccion;
