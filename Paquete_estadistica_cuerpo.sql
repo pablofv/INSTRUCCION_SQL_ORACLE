e883417679bdf93e9da7fc2d72baa4e17f5575ab
pablofv <tramitesyregistraciones@gmail.com>
2018-04-26 15:52:05 -0300

    Revert "Replica de procesos sqlserver para estadísticas en oracle"
    This reverts commit 62b7496ce57b217e0000de6ea94c4669903f30dc.

--- Paquete_estadistica_cuerpo.sql
+++ Paquete_estadistica_cuerpo.sql
@@ -358,7 +358,7 @@ create or replace package body est_paquete as
                                        join actuacion_exp a on a.id_informacion = i.id_informacion
                     where i.id_expediente = idexp
                     and   i.fecha_informacion between fechaDesde and fechaHasta
-                    and   i.id_tipo_informacion = 241 -- 241 CMC CiERRE MASIVO DE CAUSAS.
+                    and   i.id_tipo_informacion = 241
               /*      union all
                     select c.tipo_radicacion, c.id_cambio_asignacion_exp, c.fecha_asignacion, c.codigo_tipo_cambio_asignacion as codigo, c.id_expediente
                     from cambio_asignacion_exp c
