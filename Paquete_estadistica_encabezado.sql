a8a9da1ed239c0ec5f011c2ed4ee75da397cf9d6
pablofv <tramitesyregistraciones@gmail.com>
2018-05-21 09:04:37 -0300

    Replica de procesos sqlserver para estadísticas en oracle
    Proceso que genera una tabla con las actuaciones con códigos de salida
    de todos los expedientes que ingresaron en el período.

--- Paquete_estadistica_encabezado.sql
+++ Paquete_estadistica_encabezado.sql
@@ -48,6 +48,7 @@ create or replace package est_paquete as
     v_numero_estadistica int;
 
     procedure generar_est_cambio_asignacion(v_fechaDesde in timestamp, v_fechaHasta in timestamp, id_cam in int);
+    procedure generar_est_actuacion_exp(v_fechaDesde in timestamp, v_fechaHasta in timestamp, id_cam in int);
     procedure saldo_al_inicio(v_fechahasta in timestamp, id_cam in int);
     procedure saldo_multibase(v_camara in int, v_numero_ejecucion in int, v_numero_estadistica in int);
     procedure ingresados_y_reingresados(v_fechaDesde in timestamp, v_fechahasta in timestamp, id_cam in int);
