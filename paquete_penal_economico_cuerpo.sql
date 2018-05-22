fcab4b856e40cbe255aa16258b9dd9678ba69516
pablofv <tramitesyregistraciones@gmail.com>
2018-05-22 12:47:36 -0300

    Replica de procesos sqlserver para estadísticas en oracle
    Antes tenía pensado hacer dos procesos, uno con saldo inicial y otro con
    año 0.
    Ahora cambié, siguiendo la lógica de instrucción y haciendo un solo
    proceso que empiece con 0, si quiero luego puedo agregar el proceso que
    calcula el saldo inicial.
    Luego agregó el proceso para generar mis tablas de asignaciones y de
    actuaciones, elimino las falsas asignaciones de correccional, lo
    anterior a 2008.

--- paquete_penal_economico_cuerpo.sql
+++ paquete_penal_economico_cuerpo.sql
@@ -1,17 +1,17 @@
 create or replace package body est_paquete_penal_economico as
-    procedure calcular_estadistica_p_e_csi(desde in timestamp default to_timestamp('01/01/2012', 'dd/mm/yyyy'), hasta in timestamp default to_timestamp('31/12/2012', 'dd/mm/yyyy')) is
+    procedure calcular_estadistica_p_econ(desde in timestamp, hasta in timestamp) is
       error_enEjecutarProceso exception; -- excepcion para cuando quiero calcular algo que ya está calculado
-      v_proceso varchar2(30) := 'calcular_estadistica_p_e_csi';
+      v_proceso varchar2(30) := 'calcular_estadistica_p_econ';
     begin
         if EST_PAQ_EJECUTAR.ejecutar_proceso(f_desde => desde, f_hasta => hasta, CAMARA => N_CAMARA) = 1 then
             /* 1-> error, 0-> correcto */
             raise error_enEjecutarProceso;
         else
-            if not EST_PAQ_EJECUTAR.periodo_inmediato_anterior (f_desde => desde, p_CAMARA => N_CAMARA) then
-                est_paquete.saldo_al_inicio(v_fechahasta => desde, id_cam => N_CAMARA);
-            end if;
-            est_paquete.ingresados(V_FECHADESDE => desde, V_FECHAHASTA => hasta, id_cam => N_CAMARA);
-            est_paquete.reingresados(v_fechaDesde => desde, v_fechahasta => hasta, id_cam => N_CAMARA);
+            est_paquete.generar_est_cambio_asignacion(v_fechaDesde => desde, v_fechaHasta => hasta, id_cam => N_CAMARA);
+            est_paquete.ingresados_y_reingresados(V_FECHADESDE => desde, V_FECHAHASTA => hasta, id_cam => N_CAMARA);
+            eliminarAnterioresA2008;
+            eliminarFalsasAsignaciones;
+            est_paquete.generar_est_actuacion_exp(v_fechaDesde => desde, v_fechaHasta => hasta, id_cam => N_CAMARA);
             est_paquete.agrego_delito(id_cam => N_CAMARA);
             est_paquete.calcula_salidos(finPeriodo => hasta, id_cam => N_CAMARA);
         end if;
@@ -20,10 +20,41 @@ create or replace package body est_paquete_penal_economico as
           est_paquete.inserta_error(m_error => 'HUBO UN ERROR EJECUTANDO LA FUNCIÓN "EJECUTAR_PROCESO"', nombre_proceso => v_proceso);
       when others then
           est_paquete.inserta_error(m_error => DBMS_UTILITY.format_error_stack, nombre_proceso => v_proceso);
-    end calcular_estadistica_p_e_csi;
+    end calcular_estadistica_p_econ;
 
-    procedure calcular_estadistica_p_e_ssi(desde in timestamp default to_timestamp('01/01/2008', 'dd/mm/yyyy'), hasta in timestamp default to_timestamp('31/12/2008', 'dd/mm/yyyy')) is
+    procedure eliminarAnterioresA2008 is
+        v_proceso varchar2(30) := 'eliminarAnterioresA2008';
+        v_inicio timestamp := systimestamp;
+        v_fin timestamp;
     begin
-      null;
-    end calcular_estadistica_p_e_ssi;
+        /* ELIMINO AQUELLOS EXPEDIENTES QUE TIENEN AÑO DE CAUSA ANTERIOR A 2008, YA QUE LO CONSIDERABA ASÍ EN INSTRUCCIÓN CON SQLSERVER */
+        delete from est_total_a
+        where   ta_anio_exp < 2008
+        and   TA_NUMERO_DE_EJECUCION = est_paquete.v_numero_de_ejecucion;
+        commit;
+    exception
+        when others then
+            est_paquete.inserta_error(m_error => DBMS_UTILITY.format_error_stack, nombre_proceso => v_proceso);
+            rollback;
+    end eliminarAnterioresA2008;
+
+    procedure eliminarFalsasAsignaciones is
+        v_proceso varchar2(30) := 'eliminarFalsasAsignaciones';
+        v_inicio timestamp := systimestamp;
+        v_fin timestamp;
+    begin
+        delete from est_total_a a
+        where  exists (select c.id_cambio_asignacion_exp
+                       from cambio_asignacion_exp c
+                       where  c.id_cambio_asignacion_exp = A.ta_idtablaorigen
+                       and    fecha_asignacion = to_timestamp('01/03/2017 12:00:00,000000000 AM', 'DD/MM/YYYY HH12:MI:SS,FF AM')
+                       and    comentarios = 'POR ACORDADA 1/2017 CSJN'
+                       )
+        and   TA_NUMERO_DE_EJECUCION = est_paquete.v_numero_de_ejecucion;
+        commit;
+    exception
+        when others then
+            est_paquete.inserta_error(m_error => DBMS_UTILITY.format_error_stack, nombre_proceso => v_proceso);
+            rollback;
+    end eliminarFalsasAsignaciones;
 end est_paquete_penal_economico;
